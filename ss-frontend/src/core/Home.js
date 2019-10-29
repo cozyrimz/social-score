3f5d6d51364402f609f9066ef71ce9a20377344f
Sarim Shah <sarim@Sarims-MBP.attlocal.net>
2019-10-29 00:54:54 -0500

    added some styling tests and extra packages, added media picture

--- ss-frontend/src/core/Home.js
+++ ss-frontend/src/core/Home.js
@@ -8,23 +8,25 @@ function Home () {
     const handleChange = event => setURL(event.target.value)
     
     return ( <div>
-        <div className="jumbotron">
-            {/* <h1 className="display4">Home</h2> */}
-            <p className="lead">ALL YOUR PROSPECT'S ONLINE STATS....IN ONE PLACE</p>
-            <hr className="my-4"></hr>
-            <div className="input-group mb-3">
-                <input 
-                    type="text" 
-                    className="form-control" placeholder="Start By Typing your Website URL" aria-label="" aria-describedby="basic-addon2" 
-                    value ={url} 
-                    onChange={handleChange}></input>
-                <div className="input-group-append">
-                 <Link to={{pathname: "/quickstats", state: {url: url}}}>
-                    <button className="btn btn-outline-secondary" type="button">Submit</button>
-                </Link>
+        <div className="homecontainer">
+            <div className="jumbotron">
+                {/* <h1 className="display4">Home</h2> */}
+                <p className="lead">ALL YOUR PROSPECT'S ONLINE STATS....IN ONE PLACE</p>
+                <hr className="my-4"></hr>
+                <div className="input-group mb-3">
+                    <input 
+                        type="text" 
+                        className="form-control" placeholder="Start By Typing your Website URL" aria-label="" aria-describedby="basic-addon2" 
+                        value ={url} 
+                        onChange={handleChange}></input>
+                    <div className="input-group-append">
+                    <Link to={{pathname: "/quickstats", state: {url: url}}}>
+                        <button className="btn btn-outline-secondary" type="button">Submit</button>
+                    </Link>
+                    </div>
                 </div>
+                <p>{url}</p>
             </div>
-            <p>{url}</p>
         </div>
     </div>)
 };
